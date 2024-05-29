8f2d539faf24116c576ff4dd252ed62e15bd4b6d
Dereto <nobody7548@gmail.com>
2024-05-29 12:30:27 +0800

    update default port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'uploads')));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
